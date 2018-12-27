16bd8e078f858c13282845e55b63409276191f2f
MartinXPN <mirakyanmartin@gmail.com>
2018-12-27 18:27:16 +0400

    commit

---
+++ app/src/main/java/com/example/kar/horoscope_world/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.kar.horoscope_world;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent intent = new Intent( SplashScreen.this, MainActivity.class );
+                    startActivity ( intent );
+                    finish();
+                } catch (InterruptedException e ){
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
